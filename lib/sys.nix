b1b75d187dabc38a11b7e261a0641b8f27e973f2
Satria <90962949+SX-9@users.noreply.github.com>
2024-10-12 22:02:42 +0700

    re-run scan and disk clean

--- lib/sys.nix
+++ lib/sys.nix
@@ -6,6 +6,10 @@
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
     };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+    };
     optimise.automatic = true;
   };
   imports = [
