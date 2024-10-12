9d89bbd4a4259059c3236f3557f8b48bccae7dcb
Satria <90962949+SX-9@users.noreply.github.com>
2024-10-12 21:49:41 +0700

    cleanup

--- flake.nix
+++ flake.nix
@@ -8,26 +8,28 @@
     hostname = "thinkpad-l450";
     username = "satr14";
   in {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      specialArgs = {
-        inherit hostname;
-        inherit username;
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit hostname;
+          inherit username;
+        };
+        modules = [
+          ./lib/sys.nix
+          ./lib/user.nix
+        ];
       };
-      modules = [
-        ./lib/sys.nix
-        ./lib/user.nix
-      ];
-    };
-    nixosConfigurations.thinkpad = nixpkgs.lib.nixosSystem {
-      specialArgs = {
-        inherit hostname;
-        inherit username;
+      thinkpad = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit hostname;
+          inherit username;
+        };
+        modules = [
+          ./lib/sys.nix
+          ./lib/user.nix
+          ./hw/extras.nix
+        ];
       };
-      modules = [
-        ./lib/sys.nix
-        ./lib/user.nix
-        ./hw/extras.nix
-      ];
     };
   };
 }
