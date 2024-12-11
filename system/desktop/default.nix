8fa5e6ea8c4d09dabd57632e05429a7487f4be5a
SX-9 <90962949+SX-9@users.noreply.github.com>
2024-12-11 19:57:32 +0700

    libvirt qemu options

--- system/desktop/default.nix
+++ system/desktop/default.nix
@@ -54,7 +54,21 @@
   virtualisation = {
     waydroid.enable = true;
     docker.enable = true;
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        swtpm.enable = true;
+        runAsRoot = false;
+        ovmf = {
+          enable = false;
+          packages = [(pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd];
+        };
+      };
+    };
   };
 
   networking = {
