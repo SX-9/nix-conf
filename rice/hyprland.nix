9816d7bfc1437a13ce71fd6a30ca03aaa8bcab2a
satr14 <90962949+SX-9@users.noreply.github.com>
2025-06-16 21:05:10 +0700

    workspace keybinds

--- rice/hyprland.nix
+++ rice/hyprland.nix
@@ -141,8 +141,17 @@
 
         "SUPER SHIFT, right, movetoworkspace, +1"
         "SUPER SHIFT, left, movetoworkspace, -1"
+        "SUPER SHIFT, 1, movetoworkspace, 1"
+        "SUPER SHIFT, 2, movetoworkspace, 2"
+        "SUPER SHIFT, 3, movetoworkspace, 3"
+        "SUPER SHIFT, 4, movetoworkspace, 4"
+
         "SUPER, right, workspace, e+1"
         "SUPER, left, workspace, e-1"
+        "SUPER, 1, workspace, 1"
+        "SUPER, 2, workspace, 2"
+        "SUPER, 3, workspace, 3"
+        "SUPER, 4, workspace, 4"
       ];
 
       bindm = [
