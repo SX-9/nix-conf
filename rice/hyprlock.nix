0aa3b29fd693aa651a30426e0878545f5ac4f5e3
satr14 <90962949+SX-9@users.noreply.github.com>
2025-06-17 20:46:28 +0700

    hyprlock tweaks

--- rice/hyprlock.nix
+++ rice/hyprlock.nix
@@ -1,41 +1,57 @@
-{ rice, ... }: {
+{ rice, username, hostname, ... }: {
   programs.hyprlock = {
     enable = true;
     settings = {
+
       general = {
         ignore_empty_input = true;
         hide_cursor = false;
       };
+
+      animation = {
+        bezier = [
+          "easeOutQuint,0.23,1,0.32,1"
+          "quick,0.15,0,0.1,1"
+          "overshot,0.05,0.9,0.1,1.1"
+        ];
+
+        animation = [
+          "fade, 1, 3.5, easeOutQuint"
+        ];
+      };
+
       background = [
         {
           path = "screenshot";
-          brightness = 0.2;
-          blur_passes = 5;
+          brightness = 0.25;
+          blur_passes = 3;
           blur_size = 1;
         }
       ];
+
       label = [
         {
           text = "$TIME12";
           color = "$text";
-          font_size = 70;
-          position = "0, 30";
+          font_size = 64;
+          position = "0, 32";
           shadow_passes = 1;
           shadow_boost = 0.5;
           halign = "center";
           valign = "center";
         }
         {
-          text = "󰌾 Howdy, <span foreground=\"##$accentAlpha\">$USER</span>!";
+          text = "<span foreground=\"##$accentAlpha\">${username}</span>@${hostname}";
           color = "$subtext0";
-          font_size = 15;
-          position = "0, -30";
+          font_size = 16;
+          position = "0, -32";
           shadow_passes = 1;
           shadow_boost = 0.5;
           halign = "center";
           valign = "center";
         }
       ];
+
       input-field = [
         {
           size = "200, 50";
@@ -60,6 +76,7 @@
           fail_text = " $FAIL ($ATTEMPTS)";
         }
       ];
+
     };
   };
 }
