6a237cb08cf5bb157aeb725e8a1e680f23123efa
satr14 <90962949+SX-9@users.noreply.github.com>
2025-06-17 19:07:39 +0700

    remove legacy config, change bat0 to bat1

--- hardware/thinkpad.nix
+++ hardware/thinkpad.nix
@@ -7,38 +7,6 @@
     kernel.sysctl."vm.laptop_mode" = 5;
     initrd.availableKernelModules = [ "thinkpad_acpi" ];
   };
-  environment = {
-    systemPackages = [ pkgs.zcfan ]; # thinkfan replacement for L450
-    etc."zcfan.conf" = {
-      mode = "0644";
-      text = ''
-        max_temp 80
-        max_level full-speed
-
-        med_temp 70
-        med_level 7
-
-        low_temp 60
-        low_level auto
-
-        temp_hysteresis 10
-      '';
-    };
-  };
-  systemd.services.zcfan = {
-    enable = false;
-    description = "ZCFan - (backup) ThinkPad Fan Control";
-    wantedBy = [ "multi-user.target" ];
-    preStart = "
-      /run/current-system/sw/bin/modprobe -rv thinkpad_acpi && /run/current-system/sw/bin/modprobe -v thinkpad_acpi fan_control=1 experimental=1
-    ";
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.zcfan}/bin/zcfan";
-      Restart = "always";
-      RestartSec = "5s";
-    };
-  };
   services = {
     logind.extraConfig = "HandlePowerKey=ignore"; # classmates keep pressing power button while im working :<
     power-profiles-daemon.enable = false;
@@ -47,8 +15,8 @@
     tlp = {
       enable = true;
       settings = {
-        STOP_CHARGE_THRESH_BAT0 = "85";
-        START_CHARGE_THRESH_BAT0 = "80";
+        STOP_CHARGE_THRESH_BAT1 = "85";
+        START_CHARGE_THRESH_BAT1 = "80";
         CPU_BOOST_ON_AC = "1";
         TLP_DEFAULT_MODE = "BAT";
         PLATFORM_PROFILE_ON_AC = "performance";
@@ -56,7 +24,7 @@
       };
     };
     thinkfan = {
-      enable = true; # Problems with L450: https://github.com/NixOS/nixpkgs/issues/395739
+      enable = true;
       levels = [
         [ "level auto"       0  60  ]
         [ "level full-speed" 60 70  ]
