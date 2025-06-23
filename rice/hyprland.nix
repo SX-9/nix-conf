3232936fa686662b1411377db47e6dd06ef26894
satr14 <90962949+SX-9@users.noreply.github.com>
2025-06-23 20:40:26 +0700

    new shortcut

--- rice/hyprland.nix
+++ rice/hyprland.nix
@@ -184,6 +184,7 @@
         "SUPER, K, exec, notify-send -u critical Hyprland 'Focus Mode' && notify-send '(SUPER+X to reset)' && systemctl --user stop swww waybar && hyprctl --batch 'keyword decoration:inactive_opacity 1.0; keyword decoration:blur:enabled 0; keyword general:gaps_in 0; keyword general:gaps_out 0; keyword general:border_size 1; keyword decoration:rounding 0; keyword decoration:shadow:enabled false'"
         "SUPER, H, exec, notify-send Hyprland 'Animations Off' && hyprctl keyword animations:enabled 0"
         "SUPER, X, exec, dunstctl close-all && hyprctl reload && systemctl --user restart swww waybar hypridle"
+        "SUPER, Z, exec, dunstctl close-all"
 
         "SUPER SHIFT, S, exec, hyprshot -m region -o ~/Pictures/Screenshots"
         ", PRINT, exec, hyprshot -m region -o ~/Pictures/Screenshots"
